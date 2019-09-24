65168939f3dc121c786ff295cff453c6bed0a81f
djodjo3 <djodjo3@hotmail.com>
2019-09-24 12:16:29 +0000

    Adding comment about DNI fix

--- shop/prestashop-performance/pre-install-scripts/00-fixtures.sh
+++ shop/prestashop-performance/pre-install-scripts/00-fixtures.sh
@@ -7,6 +7,7 @@
                 rm -rf /var/www/html/install/fixtures/fashion/
                 mkdir -p  /var/www/html/install/fixtures/fashion/
                 cp -R /opt/"${PS_CUSTOM_FIXTURES_FOLDER}"/* /var/www/html/install/fixtures/fashion/
+		## since DNI is a mandatory field for addresses in some country, this replacement fixes missing DNI from shop-creator fixture generation
                 sed -i -e 's/dni=""/dni="11111111"/g' /var/www/html/install/fixtures/fashion/data/address.xml
         else
 		echo ""
